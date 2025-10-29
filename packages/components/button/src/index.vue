f328a24cb8398758b0f379901e190121431d6e30
Sogrey <suzhaoheng2006@163.com>
2025-10-29 10:25:47 +0800

    初始化button组件

---
+++ packages/components/button/src/index.vue
@@ -0,0 +1,15 @@
+<!-- button 按钮组件的 .vue 源代码 -->
+
+<template>
+    <button>
+        这是测试按钮
+    </button>
+</template>
+<script setup>
+
+defineOptions({
+    name: 's-button'
+})
+
+</script>
+<style scoped></style>
